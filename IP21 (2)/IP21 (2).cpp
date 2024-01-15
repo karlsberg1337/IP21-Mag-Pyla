6f27a6c33ac2dad5cf0aa8036584f4b04e41700b
a.pavlov.2022 <a.pavlov.2022@CLOUD-RDHOST1>
2024-01-15 11:40:20 +0700

    Добавьте файлы проекта.

---
+++ IP21 (2)/IP21 (2).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char masked_raider[] = "Живой!";
+	char* jimmy = masked_raider;
+	printf("Грабитель в маске сейчас %s Джимми сейчас %s\n ", masked_raider, jimmy);
+	masked_raider[0] = 'M';
+	masked_raider[1] = 'E';
+	masked_raider[2] = 'P';
+	masked_raider[3] = 'T';
+	masked_raider[4] = 'B';
+	masked_raider[5] = '!';
+	printf("Грабитель в маске сейчас %s Джимии сейчас %s\n", masked_raider, jimmy);
+	return 0;
+}
---
+++ IP21/IP21.cpp
@@ -0,0 +1,20 @@
+﻿//#include <iostream>
+#include <locale.h>
+
+void go_south_east(int *lat, int *lon)
+{
+	*lat = *lat - 1;
+	*lon = *lon + 1;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int latitude = 32;
+	int longitude = -64;
+	go_south_east(&latitude, &longitude);
+	printf("Стоп! Теперь наш координаты: [%i, %i]\n", latitude, longitude);
+	return 0;
+
+}
+
